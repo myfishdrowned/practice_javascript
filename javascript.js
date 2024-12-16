e7f1dabab725b3b5e39c64414cbe1d32400fcce6
myfishdrowned <saif.rahman.va@gmail.com>
2024-12-16 10:32:11 -0500

    practiced javascript variable and number concepts

--- javascript.js
+++ javascript.js
@@ -1,1 +1,47 @@
 console.log('greetings huzz!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let firstName = 'John'
+// let lastName = 'Pork'
+
+// const max = 57
+// const actual = max - 13
+// const percentage = actual / max
+
+// let s = "my" + ' ' + "string";
+// alert(s); // mystring
+
+// Note that if any of the operands is a string, then the other one is converted to a string too.
+
+// For example:
+
+// console.log(('1' + 2));
+// console.log((2 + '1'));
+
+// console.log((6 - '2'));
+// console.log(('6' / '2'));
+
+// let y = -2;
+// console.log((+y)); // -2
+
+// Converts non-numbers
+// console.log((+true)); // 1
+// console.log((+""));   // 0
+
+let apples = "2";
+let oranges = "3";
+
+// both values converted to numbers before the binary plus
+console.log((+apples + +oranges)); // 5
