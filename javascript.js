c56209aad142bd66ea7224b8e20a856c57db8f03
myfishdrowned <saif.rahman.va@gmail.com>
2024-12-21 14:07:21 -0500

    practiced writing basic functions in python

--- javascript.js
+++ javascript.js
@@ -115,3 +115,12 @@ console.log('greetings huzz!')
 // const cars = carBrands.concat(moreCarBrands)
 // console.log(cars)
 // console.log(cars.at(-1))
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+    text += cars[i];
+}
+console.log(text)
--- practice.py
+++ practice.py
@@ -3,7 +3,17 @@
 # number = int(input('what\'s your number? '))
 # print(add7(number))
 
-def greeting(name):
-    return f'hello, {name}'
-name = input('What\'s your name? ')
-print(greeting(name))
+# def greeting(name):
+#     return f'hello, {name}'
+# name = input('What\'s your name? ')
+# print(greeting(name))
+
+
+
+def what_cars():
+    result = ''
+    for i in cars:
+        result += i + '\n'
+    return result
+cars = ['toyota', 'honda', 'lexus', 'nissan', 'infiniti', 'mazda']
+print(what_cars())
